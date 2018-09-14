1a5aa632be099d631941d1ccb8fa6e701f6842f6
Dominic Doty <dominic.doty@colorado.edu>
2018-09-14 16:53:28 -0600

    Refactored to move LETIMER stuff into separate file and CMU initialization into another file. Probably not the best way to do it.

--- main.c
+++ main.c
@@ -80,29 +80,11 @@ uint8_t boot_to_dfu = 0;
 #include "em_core.h"
 #include "src/sleep.h"
 #include "em_letimer.h"
+#include "src/letimer.h"
 
 //***********************************************************************************
 // defined files
 //***********************************************************************************
-
-#define BLOCK_SLEEP		EM4		//EM0-4 (Note 4 isn't actually implemented)
-#define PULSE_LENGTH	175		//mS
-#define PERIOD			2250	//mS
-
-// Choose the clock based on energy mode
-#if (BLOCK_SLEEP == EM3) | (BLOCK_SLEEP == EM4)
-	#define CLOCK	cmuSelect_ULFRCO
-	#define FREQ	1000	//Hz
-//	#if PERIOD/1000 > 2147000
-//		#error Period too big, max is 2,147,000 S
-//	#endif
-#else
-	#define CLOCK	cmuSelect_LFXO
-	#define FREQ	32768	//Hz
-//	#if PERIOD/1000 > 65535
-//		#error Period too big, max is 65,535 S
-//	#endif
-#endif
 
 
 //***********************************************************************************
@@ -119,26 +101,6 @@ uint8_t boot_to_dfu = 0;
 // functions
 //***********************************************************************************
 
-void LETIMER0_IRQHandler(){
-	//disable peripheral call interrupt
-	CORE_ATOMIC_IRQ_DISABLE();
-
-	//copy the interrupt register (auto clears flag)
-	uint32_t intreg = LETIMER0->IFC;
-
-	//determine which interrupt triggered
-	if(intreg & LETIMER_IFC_UF){
-		//turn the LED off
-		GPIO_PinOutClear(LED0_port, LED0_pin);
-	}
-	else{
-		//turn the LED on
-		GPIO_PinOutSet(LED0_port, LED0_pin);
-	}
-
-	//enable peripheral call interrupt
-	CORE_ATOMIC_IRQ_ENABLE();
-}
 
 //***********************************************************************************
 // main
@@ -165,54 +127,8 @@ int main(void)
   // Initialize stack
   gecko_init(&config);
 
-  // Calculate Prescaler
-  // Note that compiler optimization just deletes this whole section and stores the values in temporary variables
-  // You will not see this run in debugger. The variables also will not show up. But the registers do get set correctly
-  uint64_t frac = (PERIOD*FREQ)/(65536*1000);	//Calculate the fraction of desired counts to max timer counts
-  uint8_t prescale = 0;
-  while(frac != 0){			//bit shift right until fraction value is zero
-	  frac = frac >> 1;		//this rounds the value up to the next power of two
-	  prescale++;			//prescale counts the number of divisions by two required to get the desired period in the LETIMER max counts
-  }							//prescale register translates this value to powers of two (ie 0->div1, 1->div2, 2->div4, etc.)
-
-  // Calculate Timer Comp Values
-  const uint16_t period_cnts = (PERIOD*FREQ)/((1<<prescale) *1000);
-  const uint16_t pulse_cnts = (PULSE_LENGTH*FREQ)/((1<<prescale) *1000);
-
-  // Configure CMU for LETIMER0
-#if CLOCK == cmuSelect_LFXO						//if because ULFRCO is automatically enabled
-  CMU_OscillatorEnable(CLOCK, true, true);		//ENABLE OSCILLATOR
-#endif
-  CMU_ClockSelectSet(cmuClock_LFA, CLOCK);		//SELECT OSC ONTO BUS
-  CMU->LFAPRESC0 = (prescale & 0b00001111);		//Clock Prescaler Set
-  CMU_ClockEnable(cmuClock_HFLE,true);			//Clock to enable register coms
-  CMU_ClockEnable(cmuClock_LETIMER0,true);		//Enable Letimer0 clock
-
-
-  // Initialize LETIMER0
-  MSC->LOCK = MSC_UNLOCK_CODE;			//Unlock the memory controller registers to allow writing
-  MSC->CTRL |= MSC_CTRL_IFCREADCLEAR;	//Set the memory controller to auto clear interrupt flags after reading them
-  MSC->LOCK = 1;						//Re-Lock the memory controller registers
-  LETIMER0->CMD = LETIMER_CMD_STOP;		//Make sure LETIMER0 is stopped
-
-  //Set up LETIMER0 config struct
-  const LETIMER_Init_TypeDef timer0 = {.enable = false,
-		  	  	  	  	  	  	  	  .comp0Top = true};
-
-  LETIMER_CompareSet(LETIMER0,0,period_cnts);	//Set the period comparator
-  LETIMER_CompareSet(LETIMER0,1,pulse_cnts);	//Set the pulse length comparator
-  LETIMER_RepeatSet(LETIMER0,0,1);				//Set rep counter to non 0 for free running mode
-  LETIMER_Init(LETIMER0,&timer0);				//Initialize timer with set values from above
-
-  // Interrupt Configuration
-  LETIMER0->IFC = LETIMER_IFC_UF | LETIMER_IFC_COMP1;	//clear int flags
-  LETIMER0->IEN = LETIMER_IEN_UF | LETIMER_IEN_COMP1;	//enable int flags
-  blockSleepMode(BLOCK_SLEEP);							//set max sleep mode
-  NVIC_EnableIRQ(LETIMER0_IRQn);						//enable interrupt vector
-
-  // Start the Timer
-  LETIMER0->CMD = LETIMER_CMD_START;	//start the LETIMER0
-  while((LETIMER0->SYNCBUSY) & 1);		//twiddle thumbs while LETIMER registers sync
+  // Initialize LETIMER
+  letimer_init();
 
   // Sleep
   while (1) {
--- src/cmu.c
+++ src/cmu.c
@@ -19,5 +19,14 @@
 void cmu_init(void){
 	// Peripheral clocks enabled
 	CMU_ClockEnable(cmuClock_GPIO, true);
+
+	// Configure CMU for LETIMER0
+	#if CLOCK == cmuSelect_LFXO						//if because ULFRCO is automatically enabled
+	  CMU_OscillatorEnable(CLOCK, true, true);		//ENABLE OSCILLATOR
+	#endif
+	CMU_ClockSelectSet(cmuClock_LFA, CLOCK);		//SELECT OSC ONTO BUS
+	CMU->LFAPRESC0 = (prescale & 0b00001111);		//Clock Prescaler Set
+	CMU_ClockEnable(cmuClock_HFLE,true);			//Clock to enable register coms
+	CMU_ClockEnable(cmuClock_LETIMER0,true);		//Enable Letimer0 clock
 }
 

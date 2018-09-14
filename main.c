036bd8bc7e46d5fdbcefba25b1ba98096044d382
Dominic Doty <dominic.doty@colorado.edu>
2018-09-14 11:27:04 -0600

    initial commit, working prescale calculation

---
+++ main.c
@@ -0,0 +1,270 @@
+
+/***********************************************************************************************//**
+ * \file   main.c
+ * \brief  Silicon Labs Empty Example Project
+ *
+ * This example demonstrates the bare minimum needed for a Blue Gecko C application
+ * that allows Over-the-Air Device Firmware Upgrading (OTA DFU). The application
+ * starts advertising after boot and restarts advertising after a connection is closed.
+ ***************************************************************************************************
+ * <b> (C) Copyright 2016 Silicon Labs, http://www.silabs.com</b>
+ ***************************************************************************************************
+ * This file is licensed under the Silabs License Agreement. See the file
+ * "Silabs_License_Agreement.txt" for details. Before using this software for
+ * any purpose, you must agree to the terms of that agreement.
+ **************************************************************************************************/
+
+/* Board headers */
+#include "init_mcu.h"
+#include "init_board.h"
+#include "ble-configuration.h"
+#include "board_features.h"
+
+/* Bluetooth stack headers */
+#include "bg_types.h"
+#include "native_gecko.h"
+#include "gatt_db.h"
+
+/* Libraries containing default Gecko configuration values */
+#include "em_emu.h"
+//#include "em_cmu.h"
+
+/* Device initialization header */
+#include "hal-config.h"
+
+#if defined(HAL_CONFIG)
+#include "bsphalconfig.h"
+#else
+#include "bspconfig.h"
+#endif
+
+/***********************************************************************************************//**
+ * @addtogroup Application
+ * @{
+ **************************************************************************************************/
+
+/***********************************************************************************************//**
+ * @addtogroup app
+ * @{
+ **************************************************************************************************/
+
+#ifndef MAX_CONNECTIONS
+#define MAX_CONNECTIONS 4
+#endif
+uint8_t bluetooth_stack_heap[DEFAULT_BLUETOOTH_HEAP(MAX_CONNECTIONS)];
+
+// Gecko configuration parameters (see gecko_configuration.h)
+static const gecko_configuration_t config = {
+  .config_flags = 0,
+  .sleep.flags = SLEEP_FLAGS_DEEP_SLEEP_ENABLE,
+  .bluetooth.max_connections = MAX_CONNECTIONS,
+  .bluetooth.heap = bluetooth_stack_heap,
+  .bluetooth.heap_size = sizeof(bluetooth_stack_heap),
+  .bluetooth.sleep_clock_accuracy = 100, // ppm
+  .gattdb = &bg_gattdb_data,
+  .ota.flags = 0,
+  .ota.device_name_len = 3,
+  .ota.device_name_ptr = "OTA",
+#if (HAL_PA_ENABLE) && defined(FEATURE_PA_HIGH_POWER)
+  .pa.config_enable = 1, // Enable high power PA
+  .pa.input = GECKO_RADIO_PA_INPUT_VBAT, // Configure PA input to VBAT
+#endif // (HAL_PA_ENABLE) && defined(FEATURE_PA_HIGH_POWER)
+};
+
+// Flag for indicating DFU Reset must be performed
+uint8_t boot_to_dfu = 0;
+//***********************************************************************************
+// Include files
+//***********************************************************************************
+
+#include "src/main.h"
+#include "src/gpio.h"
+#include "src/cmu.h"
+#include "em_core.h"
+#include "src/sleep.h"
+#include "em_letimer.h"
+
+//***********************************************************************************
+// defined files
+//***********************************************************************************
+
+#define SLEEPMODE		EM2		//EM0-4 (Note 4 isn't actually implemented)
+#define PULSE_LENGTH	175		//mS
+#define PERIOD			2250	//mS
+
+// Choose the clock based on energy mode
+#if (SLEEPMODE == EM3) | (SLEEPMODE == EM4)
+	#define CLOCK	cmuSelect_ULFRCO
+	#define FREQ	1000	//Hz
+//	#if PERIOD/1000 > 2147000
+//		#error Period too big, max is 2,147,000 S
+//	#endif
+#else
+	#define CLOCK	cmuSelect_LFXO
+	#define FREQ	32768	//Hz
+//	#if PERIOD/1000 > 65535
+//		#error Period too big, max is 65,535 S
+//	#endif
+#endif
+
+
+//***********************************************************************************
+// global variables
+//***********************************************************************************
+
+
+//***********************************************************************************
+// function prototypes
+//***********************************************************************************
+
+
+//***********************************************************************************
+// functions
+//***********************************************************************************
+/*
+void LETIMER0_IRQHandler(){
+	//disable peripheral call interrupt
+	CORE_ATOMIC_IRQ_DISABLE();
+
+	//copy the interrupt register (auto clears flag)
+	uint32_t intreg = LETIMER0->IF;
+	LETIMER0->IFC = intreg;
+
+	//determine which interrupt triggered
+	if(intreg & LETIMER_IFC_UF){
+		//turn the LED off
+		GPIO_PinOutClear(LED0_port, LED0_pin);
+	}
+	else{
+		//turn the LED on
+		GPIO_PinOutSet(LED0_port, LED0_pin);
+	}
+
+	//enable peripheral call interrupt
+	CORE_ATOMIC_IRQ_ENABLE();
+}
+*/
+
+//***********************************************************************************
+// main
+//***********************************************************************************
+
+/**
+ * @brief  Main function
+ */
+
+int main(void)
+{
+  // Initialize device
+  initMcu();
+
+  // Initialize board
+  initBoard();
+
+  // Initialize GPIO
+  gpio_init();
+
+  // Initialize clocks
+  cmu_init();
+
+  // Initialize stack
+  gecko_init(&config);
+
+  // Calculate Prescaler
+  uint64_t frac = (PERIOD*FREQ)/(65535*1000);	//Calculate the fraction of desired counts to max timer counts
+  uint32_t temp = frac;
+  uint8_t prescale = 0;
+  while(temp != 0){			//bit shift right until fraction value is zero
+	  temp = temp >> 1;		//this rounds the value up to the next power of two
+	  prescale++;			//prescale counts the number of divisions by two required to get the desired period in the LETIMER max counts
+  }							//prescale register translates this value to powers of two (ie 0->div1, 1->div2, 2->div4, etc.)
+
+  // Calculate Timer Comp Values
+  const uint16_t period_cnts = (PERIOD*FREQ)/((1<<prescale) *1000);
+  const uint16_t pulse_cnts = (PULSE_LENGTH*FREQ)/((1<<prescale) *1000);
+
+  // Configure CMU for LETIMER0
+#if CLOCK == cmuSelect_LFXO		//if because ULFRCO is automatically enabled
+  CMU_OscillatorEnable(CLOCK, true, true);		//ENABLE OSCILLATOR
+#endif
+  CMU_ClockSelectSet(cmuClock_LFA, CLOCK);		//SELECT OSC ONTO BUS
+  CMU_ClockDivSet(cmuClock_LETIMER0, prescale);	//Clock Prescaler Set
+  CMU_ClockEnable(cmuClock_HFLE,true);			//Clock to enable register coms
+  CMU_ClockEnable(cmuClock_LETIMER0,true);		//Enable Letimer0 clock
+
+
+  // Initialize LETIMER0
+//  MSC->CTRL = MSC_CTRL_IFCREADCLEAR;	//Set the memory controller to auto clear interrupt flags after reading them
+  LETIMER0->CMD = LETIMER_CMD_STOP;		//Make sure LETIMER0 is stopped
+
+  //Set up LETIMER0 config struct
+  const LETIMER_Init_TypeDef timer0 = {.enable = false,
+		  	  	  	  	  	  	  	  .comp0Top = true};
+
+  LETIMER_CompareSet(LETIMER0,0,period_cnts);	//Set the period comparator
+  LETIMER_CompareSet(LETIMER0,1,pulse_cnts);	//Set the pulse length comparator
+  LETIMER_RepeatSet(LETIMER0,0,1);				//Set rep counter to non 0 for free running mode
+  LETIMER_Init(LETIMER0,&timer0);				//Initialize timer with set values from above
+
+  // Interrupt Configuration
+//  LETIMER0->IFC = LETIMER_IFC_UF|LETIMER_IFC_COMP1;		//clear int flags
+//  LETIMER0->IEN = LETIMER_IEN_UF || LETIMER_IEN_COMP1;	//enable int flags
+//  blockSleepMode(SLEEPMODE);							//set max sleep mode
+//  NVIC_EnableIRQ(LETIMER0_IRQn);						//enable interrupt vector
+
+  // Start the Timer
+  LETIMER0->CMD = LETIMER_CMD_START;	//start the LETIMER0
+  uint32_t sync = (LETIMER0->SYNCBUSY) & 1;
+  while(sync);			//twiddle thumbs while LETIMER registers sync
+
+  // Sleep
+  while (1) {
+	  for(int i=0; i++; i<100){
+	  GPIO_PinOutSet(LED0_port, LED0_pin);
+  	  }
+	  for(int i=0; i++; i<100){
+	  GPIO_PinOutClear(LED0_port, LED0_pin);
+	  }
+  }
+}
+
+
+//int main(void)
+//{
+//  int i;
+//
+//  // Initialize device
+//  initMcu();
+//  // Initialize board
+//  initBoard();
+//
+//  /* Initialize GPIO */
+//  gpio_init();
+//
+//  // Initialize clocks
+//  cmu_init();
+//
+//  // Initialize stack
+//  gecko_init(&config);
+//
+//
+//  while (1) {
+//		for (i = 0; i < 5000000; i++);
+//		GPIO_PinOutClear(LED0_port, LED0_pin);
+//
+//		for (i = 0; i < 5000000; i++);
+//		GPIO_PinOutClear(LED1_port, LED1_pin);
+//
+//		for (i = 0; i < 20000000; i++);
+//		GPIO_PinOutSet(LED0_port, LED0_pin);
+//		GPIO_PinOutSet(LED1_port, LED1_pin);
+//  }
+//}
+
+//for 2.25 prescaler should be div 2, counts should be 36864
+//prescaler isn't setting correctly
+//comp0 setting to 8044, not correct
+
+
+/** @} (end addtogroup app) */
+/** @} (end addtogroup Application) */
---
+++ src/sleep.h
@@ -0,0 +1,58 @@
+#ifndef SLEEP_H
+#define SLEEP_H
+
+// Sleep Routine Header
+// Author D.Doty
+// Adapted from Silicon Labs Code
+// Compiled in Simplicity IDE
+// SI Labs Copyright:
+
+/* @section License*
+ * <b>(C) Copyright 2015 Silicon Labs, http://www.silabs.com</b>
+ **********************************************************************************
+ * Permission is granted to anyone to use this software for any purpose,* including commercial applications,
+ * and to alter it and redistribute it* freely, subject to the following restrictions:
+ * 1. The origin of this software must not be misrepresented; you must not*    claim that you wrote the original software.*
+ * 2. Altered source versions must be plainly marked as such, and must not be*    misrepresented as being the original software.*
+ * 3. This notice may not be removed or altered from any source distribution.**
+ * DISCLAIMER OF WARRANTY/LIMITATION OF REMEDIES: Silicon Labs has no* obligation to support this Software.
+ * Silicon Labs is providing the* Software "AS IS", with no express or implied warranties of any kind,*
+ * including, but not limited to, any implied warranties of merchantability* or fitness for any particular purpose
+ * or warranties against infringement* of any proprietary rights of a third party.**
+ * Silicon Labs will not be liable for any consequential, incidental, or* special damages,
+ * or any other relief, or for any claim by any third party,* arising from your use of this Software.
+ ***********************************************************************************/
+
+//***********************************************************************************
+// Include files
+//***********************************************************************************
+
+#include "em_core.h"
+#include "em_emu.h"
+
+//***********************************************************************************
+// defined files
+//***********************************************************************************
+
+#define EM0 0
+#define EM1 1
+#define EM2 2
+#define EM3 3
+#define EM4 4
+
+//***********************************************************************************
+// global variables
+//***********************************************************************************
+
+
+//***********************************************************************************
+// function prototypes
+//***********************************************************************************
+
+void blockSleepMode(int8_t minimumMode);
+
+void unblockSleepMode(int8_t minimumMode);
+
+void sleep();
+
+#endif
